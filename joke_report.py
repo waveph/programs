ecf9e2b3b3ccaf0595f2d26026c1eac5a7cdd77e
B Noureddine <67389427+waveph@users.noreply.github.com>
2026-07-06 23:08:14 +0100

    update

--- joke_report.py
+++ joke_report.py
@@ -7,6 +7,9 @@ with open("jokes.txt", "r") as f:
     setup_length_line = 0
     punchline_length_line = 0 
 
+    longest_setup = ""
+    shortest_punchline = None
+
 
     for line in f:
         if line == "\n": 
@@ -15,10 +18,14 @@ with open("jokes.txt", "r") as f:
             setup_number += 1
             char = line.split(". ", 1)[1].strip()
             setup_length_line += len(char)
+            if len(char) > len(longest_setup):
+                longest_setup = char
         else:
             punchline_number += 1
             char2 = line.strip()
             punchline_length_line += len(char2)
+            if shortest_punchline is None or len(char2) < len(shortest_punchline):
+                shortest_punchline = char2
 
 #            total_jokes = setup_number//2
 
@@ -29,3 +36,5 @@ with open("jokes.txt", "r") as f:
     print(f"Total of setups is : {setup_number}")
     print(f"average setup len: {avg_setup}")
     print(f"Average punchline len: {avg_punchline}")
+    print(f"The longest setup is: {longest_setup}")
+    print(f"The shortest punchline is: {shortest_punchline}")
